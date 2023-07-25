578004885350896f622479772b2d777b833406f1
hifzadoll <132747534+hifzadoll@users.noreply.github.com>
2023-07-25 13:17:04 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,22 @@
+"""x=3(3) #x will be '3'
+y= int(3) # y will be 3
+z= float(3) # z will be 3.0
+print(x)
+print(y)
+print(z)
+#type() function
+x=5
+y="hifza"
+print(type(x))
+print(type(y))"""
+# string
+x='hifza'
+print(x)
+#double qoutes are the same as the single qoute
+x="hifza"
+print(x)
+#differeent variable 
+a=4
+A='love'
+print(A)
+print(a)
